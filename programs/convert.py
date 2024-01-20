885d74008f76dfcd89f7116c7565bd5f3decd009
Russ Olsen <russ@cognitect.com>
2024-01-20 15:13:39 -0500

    A bit of a hack to get these scripts to work with the ever changing format of the questions.

--- programs/convert.py
+++ programs/convert.py
@@ -1,14 +1,17 @@
 import pypandoc
 import os
+import sys
 
-# Example file:
-docxFilename = 'General Class Pool and Syllabus 2023-2027 Public Release with Errata Feb 1 2023.docx'
-output = pypandoc.convert_file(docxFilename, 'plain', outputfile='temp.txt')
+input_path = sys.argv[1]        # Should be the docx file
+output_path = sys.argv[2]
+
+output = pypandoc.convert_file(input_path, 'plain', outputfile='temp.txt')
 assert output == ''
 
 with open('temp.txt', 'r') as outfile:
-    fixed = outfile.read().replace('\n\n', '\n').replace('~~', '')
-    with open('general.txt', 'w') as output:
+    #fixed = outfile.read().replace('\n\n', '\n').replace('~~', '')
+    fixed = outfile.read()
+    with open(output_path, 'w') as output:
         output.write(fixed)
 
 os.remove('temp.txt')
--- programs/process.rb
+++ programs/process.rb
@@ -4,6 +4,68 @@ require 'json'
 require 'yaml'
 require 'csv'
 
+class PushableFile
+  def initialize(io)
+    @io = io
+    @buffer = ''
+  end
+
+  def self.open(path, &block)
+    File.open(path) do |f|
+      pf = PushableFile.new(f)
+      block.call(pf)
+    end
+  end
+
+  def push_line(line)
+    @buffer += line
+  end
+
+  def readline
+    if @buffer.empty?
+      return @io.readline
+    else
+      temp = @buffer
+      @buffer = ''
+      return temp
+    end
+  end
+
+  def eof?
+    if not @buffer.empty?()
+      return false
+    end
+    return @io.eof?
+  end
+end
+
+def question?(line)
+  line =~ /E\d[A-Z]\d\d.*/
+end
+
+def read_text_block(io)
+  text = ""
+  while true
+    line = readline(io)
+    break if line.empty?
+    print("line::: #{line}")
+    text = "#{text}#{line} "
+  end
+  return text.strip
+end
+
+def skip_to_question(io)
+  while not io.eof?
+    line = io.readline
+    print("line: #{line}")
+    if question?(line)
+      io.push_line(line)
+      return line
+    end
+  end
+  return nil
+end
+
 def readline(io)
   text = io.readline.rstrip
   #puts("Read #{text}")
@@ -13,23 +75,24 @@ end
 def parse_header(header)
   id = header[0,5]
   correct_char = header[7,1]
+  extra = header[8,]
   correct = correct_char.ord - 'A'.ord
   [id, correct]
 end
 
 def read_header(io)
   text = readline(io)
+  read_blank(io)
   #puts "Header #{text}"
   parse_header(text)
 end
 
 def parse_answer(answer)
-  answer.sub(/^.../, '')
+  answer.sub(/^.../, '').strip
 end
 
 def read_answer(io)
-  text = readline(io)
-  #puts("answer text is #{text}")
+  text = read_text_block(io)
   parse_answer(text)
 end
 
@@ -40,24 +103,31 @@ def read_blank(io)
 end
   
 def read_question(io)
+  puts "\nquestion:"
   a=[]
   id, correct = read_header(io)
-  #puts "Id: #{id} Correct: #{correct}"
-  question = io.readline.rstrip
-  #puts "Question: #{question}"
+  puts "Id: #{id} Correct: #{correct}"
+  question = read_text_block(io)
+  puts "Question text: <<#{question}>>"
   answers = []
   answers << read_answer(io)
+  puts answers
   answers << read_answer(io)
+  puts answers
   answers << read_answer(io)
+  puts answers
   answers << read_answer(io)
-  read_blank(io)
+  puts "<<< #{answers} >>>"
+
   {id: id, correct: correct, question: question, answers: answers}
 end
 
 def read_questions(io)
   questions=[]
+  skip_to_question(io)
   until(io.eof?) do
     questions << read_question(io)
+    skip_to_question(io)
   end
   questions
 end
@@ -81,9 +151,14 @@ csv_name = "#{name}.csv"
 
 
 puts "reading #{text_name}"
+#open(text_name) do |f| 
+#  io = PushableFile.new(f)
+#  #skip_to_question(io)
+#  puts(read_questions(io))
+#end
 
 questions = nil
-open(text_name) { |f| questions = read_questions(f) }
+PushableFile.open(text_name) { |f| questions = read_questions(f) }
 
 puts "writing #{json_name}"
 open(json_name, 'w') {|f| f.puts(JSON.pretty_generate(questions))}
@@ -96,5 +171,19 @@ CSV.open(csv_name, 'w') do |csv|
   csv << ['id', 'correct', 'question', 'a', 'b', 'c', 'd']
   questions.each do |q|
     csv << hash_to_array(q)
+  end
+end
+
+open("processed_#{text_name}", 'w') do |f|
+  questions.each do |q|
+    correct = ['A', 'B', 'C', 'D'][q[:correct]]
+    f.puts("#{q[:id]} #{correct}")
+    f.puts(q[:question])
+    answer = q[:answers]
+    f.puts("A. #{answer[0]}")
+    f.puts("B. #{answer[1]}")
+    f.puts("C. #{answer[2]}")
+    f.puts("D. #{answer[3]}")
+    f.puts
   end
 end
